c6393979b422ff0cc51ed4f22cbb966551dc9890
Anatoly Ostrovsky <anatolyostrovsky@gmail.com>
2024-12-05 22:19:32 +0200

    Downgrade

--- app/resources/router/router.js
+++ app/resources/router/router.js
@@ -1,34 +1,24 @@
-import { angular } from "../node_modules/@angular-wave/angular.ts/dist/angular-ts.esm.js";
-angular.module("router", []).config([
-  "$stateProvider",
-  "$locationProvider",
-  ($stateProvider, $locationProvider) => {
-    $locationProvider.hashPrefixValue = "";
-    $locationProvider.html5Mode({
-      enabled: true,
-      requireBase: false,
-      rewriteLinks: false,
-    });
-    $stateProvider
-      .state({
-        name: "page1",
-        url: "/page1",
-        template: "<h3>Its the NG-Router hello world app!</h3>",
-      })
-      .state({
-        name: "page2",
-        url: "/page2",
-        templateUrl: "/static/router/_page2.html",
-      })
-      .state({
-        name: "page3",
-        url: "/page3",
-        templateUrl: "/hello",
-      })
-      .state({
-        name: "home",
-        url: "/static/router/router.html",
-        templateUrl: "/static/router/_home.html",
-      });
-  },
-]);
+window.angular.module("todo", ['ui.router'])
+    .config([
+        "$stateProvider", 
+        "$locationProvider",
+        "$sceDelegateProvider",
+        ($stateProvider, $locationProvider, $sceDelegateProvider) => {            
+            $locationProvider.hashPrefixValue = "";
+            $locationProvider.html5Mode({
+                enabled: true,
+                requireBase: false,
+                rewriteLinks: false,
+            });
+            $stateProvider
+            .state({
+                name: "home",
+                url: "/",
+                templateUrl: "http://10.0.2.2:8080/mobile/_home"
+            });
+            $sceDelegateProvider.resourceUrlWhitelist([
+              'self', 
+              'http://10.0.2.2:8080/**' 
+            ]);
+        }
+    ]);
